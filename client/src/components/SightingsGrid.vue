f082f239e2e2f530f02ec963c53d1e8d8c52b4ad
user <you@exampalex.com>
2019-10-29 15:19:23 +0000

    first and final commit

---
+++ client/src/components/SightingsGrid.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+	<div id="sightingsGrid">
+		<div class="sighting" v-for="sighting in sightings">
+			<h2>{{ sighting.species }}</h2>
+			<p>{{ sighting.location }} on {{ sighting.date|format }}</p>
+
+			<button>Delete Sighting</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import { eventBus } from '../main';
+import SightingsForm from './SightingsForm.vue'
+import SightingService from '@/services/SightingService.js'
+export default {
+	name: "sightings-grid",
+	props: ["sightings"],
+	filters: {
+		format(value){
+			return new Date(value).toLocaleString().substring(0, 10);
+		}
+	},
+	methods: {
+
+	},
+	// mounted() {
+	// 	SightingService.getSightings()
+	// 	.then(sightings => this.sightings = sightings);
+	//
+	// }
+}
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+}
+
+h2 {
+	padding: 0;
+	margin: 0;
+}
+
+.sighting {
+	width: 30%;
+	background: rgba(255, 255, 255, 0.7);
+	margin-bottom: 20px;
+	padding: 25px;
+}
+
+button {
+	color: #fff;
+	border: none;
+	font-size: 18px;
+	padding: 10px;
+	margin-top: 10px;
+	background: #F55536;
+}
+</style>
